e333f003802211862085f42f78ffeb785b57791f
Oliver Hoopes <ohoopes@gmail.com>
2024-06-05 13:24:50 -0700

    Update __author__ in __init__.py

--- Send_Emails/__init__.py
+++ Send_Emails/__init__.py
@@ -1,5 +1,5 @@
 # Package metadata
 __version__ = '0.1'
-__author__ = 'Your Name'
+__author__ = 'Oliver Hoopes'
 
 from .send_emails import send_email, fill_email_template, dataframe_to_html_with_style, find_user_email_by_employee_id, pull_contact_by_employee_id
