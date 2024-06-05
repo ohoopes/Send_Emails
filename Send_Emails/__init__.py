e5080a31f7ecb71ec46d72298471ca3d61fef2b7
Oliver Hoopes <ohoopes@gmail.com>
2024-06-05 13:08:45 -0700

    Update __init__.py

--- Send_Emails/__init__.py
+++ Send_Emails/__init__.py
@@ -2,4 +2,4 @@
 __version__ = '0.1'
 __author__ = 'Your Name'
 
-from .send_emails import send_emails
+from .send_emails import send_email, fill_email_template, dataframe_to_html_with_style, find_user_email_by_employee_id, pull_contact_by_employee_id
