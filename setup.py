5b973156dfc3996bff414189ce46302e4d04b58a
Oliver Hoopes <ohoopes@gmail.com>
2024-06-05 13:28:34 -0700

    changed name to lowercase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Send_Emails',
+    name='send_emails',  # Use lowercase and underscores for consistency
     version='0.1',
     packages=find_packages(),
     install_requires=[
