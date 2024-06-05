15daf4eab25f144b2029a0e7a392c83d697c057c
Oliver Hoopes <ohoopes@gmail.com>
2024-06-05 13:42:35 -0700

    ...

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Send_Smails',  # Use lowercase and underscores for consistency
+    name='Send_Emails',  # Use lowercase and underscores for consistency
     version='0.1',
     packages=find_packages(),
     install_requires=[
